09bf4b03d29528c01587d3924b501b627afca0b3
kaivalya192 <kaivalyashah192@gmail.com>
2024-10-24 11:15:58 +0530

    Revert "add"
    This reverts commit 2851f479378038062343733097a2037c2302e29a.

--- run_container.sh
+++
@@ -1,3 +0,0 @@
-docker rm -f contact-graspnet
-xhost +local:docker
-docker run --name contact-graspnet --gpus all -it -e DISPLAY=$DISPLAY -v "$(pwd)"/test_data:/contact_graspnet/test_data -v /tmp/.X11-unix:/tmp/.X11-unix  --device /dev/dri --network=host  -v "$(pwd)"/contact_graspnet:/contact_graspnet/contact_graspnet contact-graspnet -c "cd /contact_graspnet && bash"
